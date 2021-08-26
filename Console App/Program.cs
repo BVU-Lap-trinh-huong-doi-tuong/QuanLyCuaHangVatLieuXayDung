db46b266bd914f3f73d6cd2a41f6d87a24688e86
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2021-08-26 18:47:12 +0700

    Tái cấu trúc

---
+++ Console App/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+using Terminal.Gui;
+using NStack;
+
+
+namespace QuanLyCuaHangVatLieuXayDung
+{
+    class Program
+    {
+        public static void Programming(ArrayList arrVatLieu)
+        {
+            Console.Clear();
+            Menu.MenuChinh();
+            int iChucNang = int.Parse(Console.ReadLine());
+            switch (iChucNang)
+            {
+                case 0:
+                    {
+                        Console.Clear();
+                        string strTest = "Test Nhập Số Nguyên";
+                        Console.Title = strTest + " | " + Name;
+                        Console.WriteLine(strTest);
+                        int InRa = Input.NhapSoNguyen();
+                        Console.WriteLine(InRa);
+                        Console.WriteLine("Enter để tiếp tục");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 1: // Tạo đối tượng
+                    {
+                        Console.Clear();
+                        ChucNang.CreateObject(arrVatLieu);
+                        break;
+                    }
+                case 2: // Hiện đối tượng
+                    {
+                        Console.Clear();
+                        ChucNang.ShowObject(arrVatLieu);
+                        Console.WriteLine("Enter để tiếp tục");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 3: //Cập nhật đối tượng
+                    {
+                        Console.Clear();
+                        ChucNang.UpdateObject(arrVatLieu);
+                        break;
+                    }
+                case 4: //Tìm đối tượng theo tên
+                    {
+                        Console.Clear();
+                        Console.Write("Nhập từ khoá: ");
+                        string keyword = Console.ReadLine();
+                        ChucNang.ShowAllObject(ChucNang.SearchObject(arrVatLieu, keyword));
+                        Console.ReadLine();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Sai chức năng");
+                        break;
+                    }
+            }
+        }
+
+        void CatProgram()
+        {
+
+        }
+
+        void Help()
+        {
+            // Hướng Dẫn cho tạo record Cát
+
+        }
+
+        public static string Name = "Quản Lý Của Hàng Vật Liệu Xây Dựng";
+        static void Main(string[] args)
+        {
+            // For write Vietnamese
+            /* Console.OutputEncoding = Encoding.Unicode; 
+            Console.InputEncoding = Encoding.Unicode;
+            Console.Title = Name;
+
+            ArrayList arrVatLieu = new ArrayList();
+            //Run program
+            do
+            {
+                Programming(arrVatLieu);
+            } while (true);*/
+
+            Application.Init();
+            var top = Application.Top;
+
+            /*var login = new Label("Login: ") { X = 0, Y = 0 };
+            top.Add(login);*/
+
+            // Creates the top-level window to show
+            var win = new Window("MyApp")
+            {
+                X = 0,
+                Y = 1, // Leave one row for the toplevel menu
+
+                // By using Dim.Fill(), it will automatically resize without manual intervention
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+            top.Add(win);
+
+            // Creates a menubar, the item "New" has a help menu.
+            var menu = new MenuBar(new MenuBarItem[] {
+                    new MenuBarItem ("_File", new MenuItem [] {
+                        new MenuItem ("_New", "Creates new file", null),
+                        new MenuItem ("_Close", "",null),
+                        new MenuItem ("_Quit", "", () => { if (Quit ()) top.Running = false; })
+                    }),
+                    new MenuBarItem ("_Edit", new MenuItem [] {
+                        new MenuItem ("_Copy", "", null),
+                        new MenuItem ("C_ut", "", null),
+                        new MenuItem ("_Paste", "", null)
+                    })
+            });
+            top.Add(menu);
+            Application.Run();
+        }
+
+        static bool Quit()
+        {
+            var n = MessageBox.Query(50, 7, "Quit Demo", "Are you sure you want to quit this demo?", "Yes", "No");
+            return n == 0;
+        }
+    }
+}
---
+++ Windows Form/FormQuanLyCuaHangVatLieuXayDung/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace FormQuanLyCuaHangVatLieuXayDung
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(103, 93);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 26);
+            this.maskedTextBox1.TabIndex = 1;
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
